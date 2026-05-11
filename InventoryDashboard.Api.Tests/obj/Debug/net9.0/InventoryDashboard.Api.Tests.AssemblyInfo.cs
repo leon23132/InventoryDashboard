2e4b2ae5204a3b31c8cc02403b3c166dee44eea3
Leon Baltensperger <leonbaltensp@gmx.ch>
2026-05-11 11:33:34 +0200

    initial 5

--- InventoryDashboard.Api.Tests/obj/Debug/net9.0/InventoryDashboard.Api.Tests.AssemblyInfo.cs
+++ InventoryDashboard.Api.Tests/obj/Debug/net9.0/InventoryDashboard.Api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryDashboard.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06dde9fa036d0bf04a36aac937989cb0ba56170")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryDashboard.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryDashboard.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
